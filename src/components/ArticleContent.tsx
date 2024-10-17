bbe794d161e6b35932adbdb486b2e3fec874c1f5
hritik kumar <hritik.7827@gmail.com>
2024-10-17 14:00:16 +0530

    added DOMPurify to sanitize HTML before using dangerouslySetInnerHTML

--- src/components/ArticleContent.tsx
+++ src/components/ArticleContent.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import DOMPurify from "dompurify";
 
 const ArticleContent: React.FC<{ content: string }> = ({ content }) => {
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: content }} 
-    />
+    <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }} />
   );
 };
 
